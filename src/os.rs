4c232e698c2bf55ef83b2129d0cd63a454591285
Leonardo Araujo <mendes.lnr@gmail.com>
2023-02-14 17:03:46 +0200

    Removing unused function and commented line

--- src/os.rs
+++ src/os.rs
@@ -13,15 +13,6 @@ pub fn get_os_cli() -> String {
     "sh".to_string()
 }
 
-pub fn _run_command(cli: &str, full_command: &str) {
-    println!("{full_command}");
-    let mut cmd = Command::new(cli)
-        .arg(full_command)
-        .spawn()
-        .unwrap_or_else(|_| panic!("{full_command:?} failed"));
-    cmd.wait().unwrap();
-}
-
 pub fn get_child_process(cli: &str) -> Child {
     let child = Command::new(cli).stdin(Stdio::piped()).spawn().unwrap();
     child
@@ -33,7 +24,5 @@ pub fn run_command_on_child(child: &mut Child, command: &[u8]) -> Result<()> {
         .as_mut()
         .ok_or(anyhow!("Failed to capture stdin"))?
         .write_all(command)?;
-    // child.wait_with_output()?;
-
     Ok(())
 }
